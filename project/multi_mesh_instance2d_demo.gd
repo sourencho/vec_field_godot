0f2cff1ba88943217574b0b9560541700867d021
Jason Lothamer <jhlothamer@gmail.com>
2021-10-06 11:32:39 -0400

    add new export flag to distribute meshes on running demo (flag is on), added call to randomize() in this instance, and added color to mesh instances to hopefully make it easier to tell that the distribution is different on each run

--- project/multi_mesh_instance2d_demo.gd
+++ project/multi_mesh_instance2d_demo.gd
@@ -2,14 +2,16 @@ tool
 extends Node2D
 
 export var do_distribution := false setget _set_do_distribution
-
+export var distribute_on_ready := false
 
 onready var _mesh_instance = $icon
 onready var _multi_mesh_instance = $MultiMeshInstance2D
 
 
-#func _ready():
-#	_do_distribution()
+func _ready():
+	if distribute_on_ready:
+		randomize()
+		_do_distribution()
 
 
 func _do_distribution():
@@ -20,6 +22,7 @@ func _do_distribution():
 		var v = Vector2( randf() * screen_size.x, randf() * screen_size.y )
 		var t = Transform2D( 0.0, v)
 		multi_mesh.set_instance_transform_2d(i, t)
+		multi_mesh.set_instance_color(i, Color.from_hsv(randf(), randf(), max(randf(), .4), 1.0))
 
 
 func _set_do_distribution(value):
